e9f39bd6927b1211456b40dd9a0f99650b199015
ahassan-ab <52935090+ahassan-ab@users.noreply.github.com>
2019-09-09 11:58:04 -0400

    Use const (#6)
    * Use const
    
    * Update javascript_solution.js
    
    * Update javascript_solution.js

--- javascript_solution.js
+++ javascript_solution.js
@@ -1,11 +1,11 @@
-var json = require('./tests/challenge.json');
+const json = require('./tests/challenge.json');
 
 const refineParameters = data => {
   // Write your code here.
   // Remember to call this function to return the formatted json
   // with the json imported at the top of this file
 
-  // Run this file with `node your_javascript_solution.js` in your CLI to verify your answer
+  // Run this file with `node javascript_solution.js` in your CLI to verify your answer
 }
 
 // Comment out the line below to console.log and call your function to debug
--- tests/javascript_solution.test.js
+++ tests/javascript_solution.test.js
@@ -1,6 +1,6 @@
-var refineParameters = require('../javascript_solution.js');
-var challenge_json = require('./challenge.json');
-var complete_json = require('./complete.json');
+const refineParameters = require('../javascript_solution');
+const challenge_json = require('./challenge.json');
+const complete_json = require('./complete.json');
 
 test('test if the function converts the challenge json to the complete json', () => {
   expect(refineParameters(challenge_json)).toEqual(complete_json);
