92b2a0d364afbd8821c180a76cc0fbe28059f6b0
omasabdullah <46689606+omasabdullah@users.noreply.github.com>
2019-09-09 11:13:17 -0400

    add ruby challenge (#3)

---
+++ ruby_solution.rb
@@ -0,0 +1,9 @@
+require "json"
+
+def refineParameters input
+  # If you want to use the input file before running the tests, uncomment the following line
+  # input = JSON.parse(File.read(File.join(File.dirname(File.absolute_path(__FILE__)), 'tests/challenge.json')))
+
+  # Write your code here.
+  return input
+end
---
+++ tests/ruby_solution_test.rb
@@ -0,0 +1,16 @@
+# File:  tc_simple_number.rb
+
+require_relative "../ruby_solution"
+require "test/unit"
+require "json"
+
+class TestRefineParameters < Test::Unit::TestCase
+
+  # test if the function converts the challenge json to the complete json
+  def test_simple
+    solution = JSON.parse(File.read('complete.json'))
+    input = JSON.parse(File.read('challenge.json'))
+    assert_equal(refineParameters(input), solution)
+  end
+
+end
