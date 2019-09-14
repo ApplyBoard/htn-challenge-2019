b411fee99c065b9fbc24eb72c6b6d850682393f1
liew211 <samliew211@outlook.com>
2019-09-14 16:03:31 -0400

    Lightning challenge

--- python_solution.py
+++ python_solution.py
@@ -1,3 +1,20 @@
+def convert(data):
+    for x in data:
+        if type(data[x]) is dict:
+            convert(data[x])
+        elif type(data[x]) is str:
+            if data[x] == "true":
+                data[x] = True;
+            elif data[x] == "false":
+                data[x] = False;
+            elif data[x].isdigit():
+                data[x] = int(data[x])
+        else:
+            for i in range(len(data[x])):
+                if data[x][i].isdigit():
+                    data[x][i] = int(data[x][i])
+        
+
 
 def refine_parameters(data: dict):
     '''Refine the parameters of a dictionary
@@ -8,5 +25,6 @@ def refine_parameters(data: dict):
     clean_dict = {}
 
     # Write your code here.
-
+    convert(data)
+    clean_dict = data
     return clean_dict
