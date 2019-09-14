3a7111bd5b521a786da5647797273484c49abd56
aprabaka <53397707+aprabaka@users.noreply.github.com>
2019-09-14 15:32:38 -0400

    Update python_solution.py

--- python_solution.py
+++ python_solution.py
@@ -7,6 +7,18 @@ def refine_parameters(data: dict):
     '''
     clean_dict = {}
 
-    # Write your code here.
+    for i in dict:
+        if str(int(dict[x])) == dict[x]:
+            clean_dict[x] = int(dict[x])
+        elif dict[x] == "True":
+            clean_dict[x] = True
+        elif dict[x] == "False":
+            clean_dict[x] = False
+        elif isinstance(dict[x], list):
+            refine_parameters(dict[x])
+        elif isinstance(dict[x], dict):
+            refine_parameters(dict[x])
+        else:
+            clean_dict[x] = dict[x]
 
     return clean_dict
