3e8cb1b3b70a8bfd545232c64f0e20d49fa196a5
Sejin Kim <skim2257@uwo.ca>
2019-09-14 15:38:22 -0400

    h3h3 revised

--- python_solution.py
+++ python_solution.py
@@ -1,3 +1,5 @@
+bool (["false"])
+
 def refine_parameters(data_input: dict):
     '''Refine the parameters of a dictionary
 
@@ -11,8 +13,10 @@ def refine_parameters(data_input: dict):
         try:
             return int (val)
         except:
-            if val.upper () == "TRUE" or val.upper() == "FALSE":
-                return bool (val)
+            if val.upper () == "TRUE":
+                return True
+            elif val.upper() == "FALSE":
+                return False
             else:
                 return val
 
@@ -42,3 +46,40 @@ def refine_parameters(data_input: dict):
     # Write your code here.
 
     return clean_dict
+
+test = {
+    "personal_information": {
+        "first_name": "Hack",
+        "last_name": "North"
+    },
+    "grade": "90",
+    "grade_scale": "100",
+    "grading_scheme": "0",
+    "has_ca_study_permit": "true",
+    "has_us_study_permit": "true",
+    "nationality": "NP",
+    "only_direct": "false",
+    "sort_by": "relevance",
+    "student_id": "7419",
+    "student_information_id": "29990",
+    "studied_level": "grade_1",
+    "eng_test": {
+      "value": "toefl",
+      "r": "9",
+      "l": "9",
+      "s": "9",
+      "w": "9"
+    },
+    "countries": ["Canada", "USA"],
+    "school_ids": ["2", "500", "1"],
+    "school_types": ["1", "2", "3", "4"],
+    "provinces": ["Ontario", "British Columbia"],
+    "deadline": "2019-07-31",
+    "intakes": {
+      "subValue": "false",
+      "value": ["2018-12-01", "2019-01-01", "2019-02-01", "2019-03-01", "2019-04-01", "2019-07-01"]
+    }
+}
+
+
+refine_parameters (test)
