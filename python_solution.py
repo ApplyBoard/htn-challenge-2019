f2859762da9453b49e2ae8e36094c1391a16d1d0
Mehul Prajapati <mehulprajapati2802@gmail.com>
2019-09-14 12:41:50 -0700

    add solution

--- python_solution.py
+++ python_solution.py
@@ -8,5 +8,18 @@ def refine_parameters(data: dict):
     clean_dict = {}
 
     # Write your code here.
+    clean_dict = convert_to_dict(data)
+
 
     return clean_dict
+
+
+def convert():
+    
+
+
+def convert_to_dict(in_data):
+    for idx in in_data.keys():
+	in_data[idx] = convert(in_data[idx])
+
+    return in_data
