b18d1a5d3c40a64a06798dc3e9fe3919de218004
Justin Su <3387175+injust@users.noreply.github.com>
2019-09-14 19:27:19 +0000

    Wew

--- python_solution.py
+++ python_solution.py
@@ -1,3 +1,23 @@
+def numberify(item):
+    if type(item) is dict:
+        ret = {}
+        for key in item.keys():
+            ret[key] = numberify(item[key])
+        return ret
+    elif type(item) is list:
+        ret = []
+        for item in item:
+            ret.append(numberify(item))
+        return ret
+    elif type(item) is str:
+        if item == "true":
+            return True
+        elif item == "false":
+            return False
+        elif item.isdigit():
+            return int(item)
+        return item
+
 
 def refine_parameters(data: dict):
     '''Refine the parameters of a dictionary
@@ -5,8 +25,7 @@ def refine_parameters(data: dict):
     Returns:
         Return a dictionary of cleaned input
     '''
-    clean_dict = {}
 
-    # Write your code here.
+    clean_dict = numberify(data)
 
     return clean_dict
