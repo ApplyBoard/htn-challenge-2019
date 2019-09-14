b759bc7fea38e40b27d6747a78ca29b6f605fd5f
Omas Abdullah <omas.abdullah@applyboard.com>
2019-09-14 14:49:10 -0400

    This should fail

--- python_solution.py
+++ python_solution.py
@@ -1,12 +1,33 @@
 
+def convert_to_datatype(data: str):
+    if data == "true":
+        return True
+    elif data == "false":
+        return False
+    elif type(data) is list:
+        for i, entry in enumerate(data):
+            data[i] = convert_to_datatype(entry)
+        return data
+    else:
+        try:
+            intdata = int(data)
+            return intdata
+        except ValueError:
+            # just a string
+            return data
+
 def refine_parameters(data: dict):
     '''Refine the parameters of a dictionary
-
     Returns:
         Return a dictionary of cleaned input
     '''
     clean_dict = {}
 
-    # Write your code here.
+    for entry in data:
+        if type(data[entry]) is dict:
+            clean_dict[entry] = refine_parameters(data[entry])
+        else:
+            clean_dict[entry] = convert_to_datatype(data[entry])
 
+                
     return clean_dict
