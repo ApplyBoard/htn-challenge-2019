c0d29d0b8cafdffbf439c59d526384fb1ac9ac3b
dudemario <victorjsun@gmail.com>
2019-09-14 15:25:18 -0400

    Update javascript_solution.js

--- javascript_solution.js
+++ javascript_solution.js
@@ -1,6 +1,29 @@
 const json = require('./tests/challenge.json');
 
+function convertToType(item) {
+  if (isNaN(item)) {
+    if (item.toUpperCase() === 'TRUE' || item.toUpperCase() === 'FALSE') {
+      return (item.toUpperCase() === 'TRUE')
+    } else {
+      return item
+    }
+  } else if (Array.isArray(item)){
+    return item.forEach(convertToType(item))
+  } else {
+    return Number(item)
+  }
+}
+
 const refineParameters = data => {
+  let ar = Object.entries(data)
+  ar.forEach((pair)=>{
+    if (typeof(pair[1]) === 'object') {
+      data[pair[0]] = refineParameters(pair[1])
+    } else {
+      data[pair[0]] = convertToType(pair[1])
+    }
+  })
+  return data
   // Write your code here.
   // Remember to call this function to return the formatted json
   // with the json imported at the top of this file
