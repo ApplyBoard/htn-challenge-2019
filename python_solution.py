1005e309ddf251bf5383dd9a020fae582c54c198
Tao Sophia <taos1@mcmaster.ca>
2019-09-14 15:36:10 -0400

    k

--- python_solution.py
+++ python_solution.py
@@ -7,6 +7,34 @@ def refine_parameters(data: dict):
     '''
     clean_dict = {}
 
-    # Write your code here.
+    for key, value in data.items():
+        clean_dict[key] = convert(value)
 
     return clean_dict
+
+def convert(value):
+    new_value = value
+    if isinstance(value, list):
+        new_value = []
+        for nested_value in value:
+            new_value.append(convert(nested_value))
+    elif isinstance(value, dict):
+        new_value = {}
+        for nested_key, nested_value in value.items():
+            new_value[nested_key] = convert(nested_value)
+    else:
+        new_value = type_cast(value)
+    return new_value
+
+def type_cast(value: str):
+    if value.lower() == 'true':
+        return True
+    if value.lower() == 'false':
+        return False
+    try:
+        return int(value)
+    except ValueError:
+        try:
+            return float(value)
+        except ValueError:
+            return value
