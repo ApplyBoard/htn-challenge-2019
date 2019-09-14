f7e8ea3c78cdb21296828cca1a2bae15f977f9fa
Ben Taylor <bentaylor@bentaylor.me>
2019-09-14 15:16:10 -0400

    submit 2

--- javascript_solution.js
+++ javascript_solution.js
@@ -1,6 +1,11 @@
 const json = require('./tests/challenge.json');
 
-const parseBoolean = (b) => b === "true";
+const isBoolean = (string) => string === 'true' || string === 'false';
+const isNumber = (string) => /^[0-9]+$/.test(string);
+const isArray = (object) => Array.isArray(object);
+const isObject = (object) => typeof(object) === 'object'
+
+const parseBoolean = (b) => b === 'true';
 
 const refineParameters = data => {
   // Write your code here.
@@ -8,22 +13,17 @@ const refineParameters = data => {
   // with the json imported at the top of this file
 
   // Run this file with `node javascript_solution.js` in your CLI to verify your answer
-
-  data.grade = Number(data.grade);
-  data.grade_scale = Number(data.grade_scale);
-  data.grading_scheme = Number(data.grading_scheme);
-  data.has_ca_study_permit = parseBoolean(data.has_ca_study_permit);
-  data.has_us_study_permit = parseBoolean(data.has_us_study_permit);
-  data.only_direct = parseBoolean(data.only_direct);
-  data.student_id = Number(data.student_id);
-  data.student_information_id = Number(data.student_information_id);
-  data.eng_test.r = Number(data.eng_test.r);
-  data.eng_test.l = Number(data.eng_test.l);
-  data.eng_test.s = Number(data.eng_test.s);
-  data.eng_test.w = Number(data.eng_test.w);
-  data.school_ids = data.school_ids.map(n => Number(n));
-  data.school_types = data.school_types.map(n => Number(n));
-  data.intakes.subValue = parseBoolean(data.intakes.subValue);
+  if (isBoolean(data)) {
+    return parseBoolean(data);
+  } else if (isNumber(data)) {
+    return parseInt(data);
+  } else if (isArray(data)) {
+    return data.map(v => refineParameters(v));
+  } else if (isObject(data)) {
+    for (const key in data) {
+      data[key] = refineParameters(data[key]);
+    }
+  }
 
   return data;
 }
