a2d5ee47a6ba89e088808f41807534acc782439f
Richard Yang <richyang510@gmail.com>
2019-09-14 15:40:36 -0400

    Python solution

--- python_solution.py
+++ python_solution.py
@@ -1,4 +1,3 @@
-
 def refine_parameters(data: dict):
     '''Refine the parameters of a dictionary
 
@@ -7,6 +6,21 @@ def refine_parameters(data: dict):
     '''
     clean_dict = {}
 
-    # Write your code here.
+    for key, value in data.items():
+        if type(value) is dict:
+            clean_dict[key] = refine_parameters(value)
+        elif type(value) is list:
+            try:
+                clean_dict[key] = [int(val) for val in value]
+            except:
+                clean_dict[key] = value
+        else:
+            if value == "true" or value == "false":
+                clean_dict[key] = value == "true"
+            else:
+                try:
+                    clean_dict[key] = int(value)
+                except:
+                    clean_dict[key] = value
 
     return clean_dict
