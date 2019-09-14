e88d82b037767216e2bd2e974f7bba17db9a2bce
Nancy Zhao <nancy.zhao@outlook.com>
2019-09-14 15:28:30 -0400

    python pass test

--- python_solution.py
+++ python_solution.py
@@ -1,3 +1,21 @@
+# Nancy Zhao
+
+def clean_value(val: str):
+    if val == "true":
+        return True
+    elif val == "false":
+        return False
+    elif "." in val:
+        try:
+            return float(val)
+        except ValueError:
+            pass
+    else:
+        try:
+            return int(val)
+        except ValueError:
+            return val
+
 
 def refine_parameters(data: dict):
     '''Refine the parameters of a dictionary
@@ -8,5 +26,14 @@ def refine_parameters(data: dict):
     clean_dict = {}
 
     # Write your code here.
-
+    for key in data:
+        if isinstance(data[key], dict):
+            clean_dict[key] = refine_parameters(data[key])
+        elif isinstance(data[key], list):
+            clean_dict[key] = []
+            for item in data[key]:
+                item = clean_value(item)
+                clean_dict[key].append(item)
+        else:
+            clean_dict[key] = clean_value(data[key])
     return clean_dict
