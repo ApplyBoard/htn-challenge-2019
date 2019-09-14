0535155a86053d2d894753ad4edb6718a472c092
ANDREYDEN <andrey_den@ukr.net>
2019-09-14 15:48:35 -0400

    my solution

--- python_solution.py
+++ python_solution.py
@@ -1,12 +1,36 @@
-
+def convert(string: str):
+    # check for bools
+    if string == 'true':
+        return True
+    if string == 'false':
+        return False
+    
+    # check for ints and strings
+    try:
+        number = int(string)
+        if str(number) == string:
+            return number
+        else:
+            return string
+    except (TypeError, ValueError) as e:
+        return string
+    
 def refine_parameters(data: dict):
-    '''Refine the parameters of a dictionary
-
-    Returns:
-        Return a dictionary of cleaned input
-    '''
+    
     clean_dict = {}
 
-    # Write your code here.
+    for key, val in data.items():
+        # if we encounter a dictionary - refine that
+        if type(val) == dict:
+            clean_dict[key] = refine_parameters(val)
+            continue
+
+        # if we encounter a list - convert each element
+        if type(val) == list:
+            clean_dict[key] = list(map(convert, val))
+            continue
+
+        # if we encounter anything else - convert that
+        clean_dict[key] = convert(val)
 
     return clean_dict
