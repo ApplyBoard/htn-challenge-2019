1362b59dc565cfcfddf07322abc72920e0a42452
Lawrence Pang <lawrencepang36@gmail.com>
2019-09-14 15:24:50 -0400

    solution

--- python_solution.py
+++ python_solution.py
@@ -1,3 +1,15 @@
+def convert_item(item):
+    if item == 'false':
+        return False
+    elif item == 'true':
+        return True
+    try:
+        return int(item)
+    except:
+        try:
+            return float(item)
+        except:
+            return item
 
 def refine_parameters(data: dict):
     '''Refine the parameters of a dictionary
@@ -5,8 +17,18 @@ def refine_parameters(data: dict):
     Returns:
         Return a dictionary of cleaned input
     '''
+    if type(data) != dict:
+        return convert_item(data)
+
     clean_dict = {}
 
     # Write your code here.
+    for k, v in data.items():
+        if type(v) == dict:
+            clean_dict[k] = refine_parameters(v)
+        elif type(v) == list:
+            clean_dict[k] = [refine_parameters(i) for i in v]
+        else:
+            clean_dict[k] = convert_item(v)
 
     return clean_dict
