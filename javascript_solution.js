4186f877dacb95cf5458c1ca3b0143c86d3ef44c
Jim Yang <jim@Jims-MacBook-Pro.local>
2019-09-14 15:52:04 -0400

    soln

--- javascript_solution.js
+++ javascript_solution.js
@@ -6,10 +6,30 @@ const refineParameters = data => {
   // with the json imported at the top of this file
 
   // Run this file with `node javascript_solution.js` in your CLI to verify your answer
+
+  // Recursively iterate through every single possible key of the object
+  Object.keys(data).forEach( (key) => {
+    if(typeof data[key] === 'object'){
+      return refineParameters(data[key])
+    }
+    data[key] = parseData(data[key])
+  })
+  return json
 }
 
+const parseData = (data) => {
+  let newData
+  try{
+    // JSON.parse() can format either numbers or booleans
+    // If it fails, it means that the data should be a string
+    newData = JSON.parse(data)
+  } catch {
+    newData = data
+  }
+  return newData
+}
 // Comment out the line below to console.log and call your function to debug
-// console.log('formattedJson: ', refineParameters(json));
+console.log('formattedJson: ', refineParameters(json));
 
 // ----- Do not modify anything below this line (needed for test suite) ------
 module.exports = refineParameters;
