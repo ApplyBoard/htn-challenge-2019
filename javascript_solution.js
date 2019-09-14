f389286e7de9d3ddb91d831b2d36d18eaa3d797f
Zoravur Singh <zorvyy@v1040-wn-rt-a-40-214.campus-dynamic.uwaterloo.ca>
2019-09-14 15:32:42 -0400

    improve comments

--- javascript_solution.js
+++ javascript_solution.js
@@ -3,16 +3,15 @@ const json = require('./tests/challenge.json');
 /**
  * 
  * Why reinvent the wheel?
- * JSON.stringify() provides a replacer as the second argument, which iterates over key-value pairs.
- * In the case that the value is meant to be boolean or a number, 
- * we replace it with the corresponding value.
+ * JSON.stringify() provides a replacer as the second argument, 
+ * which iterates recursively over key-value pairs. In the case 
+ * that the value is meant to be boolean or a number, we replace 
+ * it with the corresponding value. Otherwise, return the value as-is.
  * Further reading: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
  * 
  * @param {string} _ The key of the json key value pair to transform
  * @param {*} value The value to be converted into the appropriate datatype.
- * 
- * 
- * 
+ * @returns The transformed value.
  */
 function _replacer(_, value) {
   if (value === "true") return true;
