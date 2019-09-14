8c1bf18cb4f4454dc06ff41f8bde04aef815f5ae
Justin Su <3387175+injust@users.noreply.github.com>
2019-09-14 19:33:50 +0000

    Some clarity improvements

--- python_solution.py
+++ python_solution.py
@@ -1,34 +1,3 @@
-def numberify(item):
-    # Parse dictionaries
-    if type(item) is dict:
-        ret = {}
-        for key in item.keys():
-            ret[key] = numberify(item[key])
-        return ret
-
-    # Parse lists
-    elif type(item) is list:
-        ret = []
-        for value in item:
-            ret.append(numberify(value))
-        return ret
-
-    # Parse strings
-    elif type(item) is str:
-        # Handle booleans
-        if item.lower() == "true":
-            return True
-        elif item.lower() == "false":
-            return False
-
-        # Handle integer values
-        elif item.isdigit():
-            return int(item)
-
-        # No conversion, return string
-        return item
-
-
 def refine_parameters(data: dict):
     '''Refine the parameters of a dictionary
 
@@ -36,6 +5,38 @@ def refine_parameters(data: dict):
         Return a dictionary of cleaned input
     '''
 
-    clean_dict = numberify(data)
 
+    '''Helper function to recursively parse the object'''
+    def numberify(item):
+        # Parse dictionaries
+        if type(item) is dict:
+            ret = {}
+            for key in item.keys():
+                ret[key] = numberify(item[key])  # Recurse
+            return ret
+
+        # Parse lists
+        elif type(item) is list:
+            ret = []
+            for value in item:
+                ret.append(numberify(value))  # Recurse
+            return ret
+
+        # Parse strings
+        elif type(item) is str:
+            # Handle booleans
+            if item.lower() == "true":
+                return True
+            elif item.lower() == "false":
+                return False
+
+            # Handle integer values
+            elif item.isdigit():
+                return int(item)
+
+            # No conversion, return string
+            return item
+
+
+    clean_dict = numberify(data)
     return clean_dict
