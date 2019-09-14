e767960baa9cae138b0e8acb1a9489fe4270d940
aprabaka <53397707+aprabaka@users.noreply.github.com>
2019-09-14 15:41:47 -0400

    Update python_solution.py

--- python_solution.py
+++ python_solution.py
@@ -7,7 +7,7 @@ def refine_parameters(data: dict):
     '''
     clean_dict = {}
 
-    for key in dict:
+    for key, value in dict.items():
         if str(int(dict[x])) == dict[x]:
             clean_dict[x] = int(dict[x])
         elif dict[x] == "True":
