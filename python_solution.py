114e8132ab868e1a44a22d726ca8d880dd4ea651
cindywang328 <31370481+cindywang328@users.noreply.github.com>
2019-09-14 15:43:52 -0400

    it passed my pytest...

--- python_solution.py
+++ python_solution.py
@@ -1,12 +1,14 @@
 
-def refine_parameters(data: dict):
-    '''Refine the parameters of a dictionary
-
-    Returns:
-        Return a dictionary of cleaned input
-    '''
+def refine_val(x):
+    return int(x) if x.isnumeric() else True if x == "true" else False if x == "false" else x
+def refine_parameters(data):
     clean_dict = {}
-
-    # Write your code here.
+    for key, value in data.iteritems():
+        if type(value) == dict:
+            clean_dict[key] = refine_parameters(value)
+        elif type(value) == list:
+            clean_dict[key] = [refine_val(x) for x in value] 
+        else:
+            clean_dict[key] = refine_val(value)
 
     return clean_dict
